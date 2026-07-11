c61c301ce18b26da71f2b4f66fceb33c52795129
ochigavarati <ochigavarati85@gmail.com>
2026-07-12 00:25:49 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from ext import db
 from routes import main
@@ -5,18 +6,21 @@ from routes import main
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "HAUSDUASDASJJIDYAW!@#*!@YBRYAYTYSD123"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aromamatch.db'
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'aromamatch.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+with app.app_context():
+    if not os.path.exists(os.path.join(basedir, 'instance')):
+        os.makedirs(os.path.join(basedir, 'instance'))
+    db.create_all()
+
 app.register_blueprint(main)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-
-    import os
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
